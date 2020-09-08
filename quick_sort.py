b0b1e96855edff5c526be381f254576d9d25db74
Jackie <40929706+zhuqingqi@users.noreply.github.com>
2020-09-08 17:32:23 +0800

    Add files via upload

--- quick_sort.py
+++ quick_sort.py
@@ -5,35 +5,36 @@ import random
 import copy
 
 
-def _quick_sort(li, left, right):
-    if left < right: #至少两个元素
-        mid = partition(li, left, right)
-        _quick_sort(li, left, mid-1)
-        _quick_sort(li, mid+1, right)
-    return li
+def _quick_sort(data, left, right):
+    if left < right:  # 至少两个元素
+        mid = partition(data, left, right)
+        _quick_sort(data, left, mid - 1)
+        _quick_sort(data, mid + 1, right)
+    return data
 
 
-def partition(li, left, right):
-    tmp = li[left]
+def partition(data, left, right):
+    tmp = data[left]
     while left < right:
-        while left < right and li[right] >= tmp:#从右边找小于tmp的数，放到左边
+        while left < right and data[right] >= tmp:  # 从右边找小于tmp的数，放到左边
             right -= 1
-        li[left] = li[right]
-        while left < right and li[left] <= tmp:#从左边找大于tmp的数，放到右边
+        data[left] = data[right]
+        while left < right and data[left] <= tmp:  # 从左边找大于tmp的数，放到右边
             left += 1
-        li[right] = li[left] #tmp归位
-    li[left] = tmp
+        data[right] = data[left]  # tmp归位
+    data[left] = tmp
     return left
 
 
 @cal_time
-def quick_sort(li):
-    _quick_sort(li, 0, len(li)-1)
-    return li
+def quick_sort(data):
+    _quick_sort(data, 0, len(data) - 1)
+    return data
+
 
 li = list(range(100000))
 random.shuffle(li)
-#print(li)
+# print(li)
 
 li1 = copy.deepcopy(li)
 print(quick_sort(li1))
