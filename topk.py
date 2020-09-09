64325cb18b8e6e6537eda1d6672eb9b5e48a81b2
Jackie <40929706+zhuqingqi@users.noreply.github.com>
2020-09-09 11:18:59 +0800

    Add files via upload

--- topk.py
+++ topk.py
@@ -1,7 +1,7 @@
 import random
 
 
-def sift(data, low, high): # sift：筛选     农村包围城市
+def sift(data, low, high):  # 建立小根堆，即小的数放到堆顶，出数的时候从堆底出，则从大到小排序
     """
     :param data: 列表
     :param low: 堆的根节点位置
@@ -16,7 +16,7 @@ def sift(data, low, high): # sift：筛选     农村包围城市
             j = j + 1
         if data[j] < tmp:  # 若子节点比父节点小,则交换父子节点的值
             data[i] = data[j]
-            i = j # 往下走
+            i = j  # 往下走
             j = i * 2 + 1
         else:
             break
@@ -25,13 +25,13 @@ def sift(data, low, high): # sift：筛选     农村包围城市
 
 def topk(data, k):
     heap = data[0:k]
-    for i in range((k-2)//2, -1, -1):  # 1.建立小根堆
+    for i in range((k-2)//2, -1, -1):  # 1.用data的前k个数建立小根堆，暂称为k堆
         sift(heap, i, k-1)
     for i in range(k, len(data)-1):
-        if heap[0] < data[i]:  # 若第i个大于堆顶元素,则把第i给元素放到堆顶
-            heap[0] = data[i]
-            sift(heap, 0, k-1)  #然后再建立新的小根堆
-    for i in range(k-1, -1, -1):  # 出数
+        if heap[0] < data[i]:  # 2.若data的第i个大于k堆底元素,则把第i个元素放到k堆底
+            heap[0] = data[i]  #
+            sift(heap, 0, k-1)  # 3.重建k堆为小根堆
+    for i in range(k-1, -1, -1):  # 只出前k个数
         heap[0], heap[i] = heap[i], heap[0]
         sift(heap, 0, i-1)
     return heap
